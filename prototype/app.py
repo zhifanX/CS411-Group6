67eb4eb227c8e40e9ca15b9df88c30c26002136f
yuyangz <yuyangz@bu.edu>
2021-11-12 02:13:23 -0500

    Running api with home and result pages

--- prototype/app.py
+++ prototype/app.py
@@ -1,7 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
+import configparser
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def weather_search():
+    return render_template('home.html')
+
+@app.route('/results', methods = ['POST'])
+def results():
+    zip_code = request.form['zipcode']
+    api_key = get_api_key()
+    data = get_weather(zip_code, api_key)
+    temp = "{0:.2f}".format(data['main']['temp'])
+    weather = data["weather"][0]["main"]
+    location = data["name"]
+    return render_template('results.html', location = location, temp = temp, weather = weather) 
+
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['api']
+    
+def get_weather(zip, api_key):
+    api_url = "http://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zip, api_key)
+    r = requests.get(api_url)
+    return r.json()
+
+if __name__ == '__main__':
+    app.run()
+    
+ 
