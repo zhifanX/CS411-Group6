1050e28a9ffb330b2124e07fc412b5b093ab0388
yuyangz <yuyangz@bu.edu>
2021-12-05 15:01:29 -0500

    Seperated flask and api files

--- prototype/app.py
+++ prototype/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
-import requests
-import configparser
+from util import weather_api
 
 app = Flask(__name__)
 
@@ -11,24 +10,13 @@ def weather_search():
 @app.route('/results', methods = ['POST'])
 def results():
     zip_code = request.form['zipcode']
-    api_key = get_api_key()
-    data = get_weather(zip_code, api_key)
+    api_key = weather_api.get_api_key()
+    data = weather_api.get_weather(zip_code, api_key)
     temp = int(data['main']['temp'])
     weather = data["weather"][0]["main"]
     location = data["name"]
-    return render_template('results.html', location = location, temp = temp, weather = weather) 
+    return render_template('results.html', location = location, temp = temp, weather = weather)
 
-def get_api_key():
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    return config['openweathermap']['api']
-    
-def get_weather(zip, api_key):
-    api_url = "http://api.openweathermap.org/data/2.5/weather?zip={}&units=imperial&appid={}".format(zip, api_key)
-    r = requests.get(api_url)
-    return r.json()
 
 if __name__ == '__main__':
     app.run()
-    
- 
