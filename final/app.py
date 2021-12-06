b547b52a09cd412b883513f1df2cab62369a19d3
yuyangz <yuyangz@bu.edu>
2021-12-06 04:20:31 -0500

    Updated tables and templates

--- final/app.py
+++ final/app.py
@@ -10,14 +10,22 @@ def weather_search():
 
 @app.route('/results', methods = ['POST'])
 def results():
+    names = []
+    links = []
+    rating = []
     zip_code = request.form['zipcode']
+    radius = request.form['radius']
     api_key = weather_api.get_api_key()
     data = weather_api.get_weather(zip_code, api_key)
     temp = int(data['main']['temp'])
     weather = data["weather"][0]["main"]
     location = data["name"]
-    restaurant = yelp_api.businesses(zip_code)
-    return render_template('results.html', location = location, temp = temp, weather = weather, restaurants = restaurant)
+    restaurant = yelp_api.businesses(radius, zip_code)
+    for biz in restaurant:
+        names.append(biz["name"])
+        links.append(biz["url"])
+        rating.append(biz['rating'])
+    return render_template('results.html', location = location, temp = temp, weather = weather, restaurants = names, links = links, rating = rating)
 
 
 if __name__ == '__main__':
--- final/util/yelp_api.py
+++ final/util/yelp_api.py
@@ -3,6 +3,7 @@ import configparser
 
 
 zip_code = 0
+radius = 0
 
 def get_api_key():
     config = configparser.ConfigParser()
@@ -10,16 +11,19 @@ def get_api_key():
     return config['yelp']['yelp_api']
 
 
-def businesses(zip):
+def businesses(rad, zip):
     global zip_code
     global PARAMETERS
+    global radius
     zip_code = zip
     list = []
     PARAMETERS['location'] = zip_code
+    PARAMETERS['radius'] = rad
     data = requests.get(url = ENDPOINT, params = PARAMETERS, headers = HEADERS).json()
     for businesses in data['businesses']:
-        list.append(businesses['name'])
+        list.append(businesses)
     return list
+
 
 ENDPOINT = 'https://api.yelp.com/v3/businesses/search'
 HEADERS = {'Authorization': 'bearer %s' % get_api_key()}
